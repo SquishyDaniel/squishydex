4190961f8ea2c961cd4c6266bbf1b846d90b1333
daniel <103461271+SquishyDaniel@users.noreply.github.com>
2026-09-23 15:34:14 +0200

    Store actual Pokemon not their reference

--- js/main.js
+++ js/main.js
@@ -2,9 +2,9 @@ const url = "https://pokeapi.co/api/v2/pokemon";
 let pokemon_list = [];
 const overview_element = document.querySelector("#overview");
 
-async function fetch_from_url() {
+async function fetch_from_url(url_to_fetch) {
     try {  
-        const response = await fetch(url);
+        const response = await fetch(url_to_fetch);
 
         if(!response.ok) {
             throw new Error(`Response status: ${response.status}`);
@@ -17,6 +17,8 @@ async function fetch_from_url() {
 }
 
 async function fetch_json_from_url(url_to_fetch) {
+    console.log(`Fetching '${url_to_fetch}'`)
+
     let response = await fetch_from_url(url_to_fetch);
 
     let data = await response.json();
@@ -34,14 +36,18 @@ function create_pokemon_overview() {
     }
 }
 
-async function initialize() {
-  const result = await fetch_json_from_url(url);
-  const fetched_list = result.results;
-  
-  for (var species in fetched_list) {
-    pokemon_list.push(fetched_list[species]);
-  }
+async function get_pokemon() {
+    const result = await fetch_json_from_url(url);
+    const fetched_list = result.results;
 
+    for (var species in fetched_list) {
+        var fetched_pokemon = await fetch_json_from_url(fetched_list[species].url);
+        pokemon_list.push(fetched_pokemon);
+    }
+}
+
+async function initialize() {
+  get_pokemon();
   create_pokemon_overview();
 }
 
