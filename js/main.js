2b1f86cc8d926e555982562066b7a78d9ef8a1c7
daniel <103461271+SquishyDaniel@users.noreply.github.com>
2026-09-23 15:08:21 +0200

    Fetching list of Pokemon

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
-const url = "https://pokeapi.co/api/v2/pokemon/ditto";
+const url = "https://pokeapi.co/api/v2/pokemon";
+let pokemon_list = [];
+const overview_element = document.querySelector("#overview");
 
-async function fetch_from_api() {
+async function fetch_from_url() {
     try {  
         const response = await fetch(url);
 
@@ -8,12 +10,39 @@ async function fetch_from_api() {
             throw new Error(`Response status: ${response.status}`);
         }
 
-        const result = await response.json();
-        
-        console.log(result.name);
+        return response;
     } catch(error){
        console.error(error.message); 
     }
 }
 
-fetch_from_api();
+async function fetch_json_from_url(url_to_fetch) {
+    let response = await fetch_from_url(url_to_fetch);
+
+    let data = await response.json();
+
+    console.log(data);
+
+    return data;
+}
+
+function create_pokemon_overview() {
+    overview_element.innerHTML = "";
+
+    for(var pokemon in pokemon_list) {
+        console.log(pokemon_list[pokemon]);
+    }
+}
+
+async function initialize() {
+  const result = await fetch_json_from_url(url);
+  const fetched_list = result.results;
+  
+  for (var species in fetched_list) {
+    pokemon_list.push(fetched_list[species]);
+  }
+
+  create_pokemon_overview();
+}
+
+initialize();
